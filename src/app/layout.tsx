d5a464ba836434ea0f5e31cd23a9ac2025f95a73
anvinssb@gmail.com <anvinssb@gmail.com>
2025-06-18 12:35:39 +0530

    theme provider and navbar components added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Navbar from './components/Navbar';
+import { ThemeProvider } from './context/ThemeContext';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -26,11 +27,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-900 text-white`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased white transition-colors dark:bg-gray-900 dark:text-white`}
       >
-        <Navbar />
-        {children}
-        <footer>footer</footer>
+        <ThemeProvider>
+          <Navbar />
+          {children}
+          <footer>footer</footer>
+        </ThemeProvider>
       </body>
     </html>
   );
